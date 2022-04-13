2e4a03d51ae98d3c71b5ad3afe15ee38b88d3531
Naila I. Bunyatova <nailaib@code.local>
2022-04-13 18:29:47 +0400

    homework1

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 7;
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("eded cutdur");
+            }
+            else
+                Console.WriteLine("eded tekdir");
+
+        }
+    }
+}
